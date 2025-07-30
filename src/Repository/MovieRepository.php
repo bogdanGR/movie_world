8ca36f2fdfb91b03d2fab2960f48e83cd5d539fa
bogdanGR <fant1k450@gmail.com>
2025-07-30 15:40:49 +0300

    remove commented code

--- src/Repository/MovieRepository.php
+++ src/Repository/MovieRepository.php
@@ -30,58 +30,33 @@ class MovieRepository extends ServiceEntityRepository
      */
     public function getSortedQueryBuilder(string $sortBy = 'created_at', string $sortOrder = 'desc'): QueryBuilder
     {
-        $qb = $this->createQueryBuilder('m');
+        $queryBuilder = $this->createQueryBuilder('m');
 
         switch ($sortBy) {
             case 'likes':
-                $qb->leftJoin('m.votes', 'v', Join::WITH, 'v.type = :type')
+                $queryBuilder->leftJoin('m.votes', 'v', Join::WITH, 'v.type = :type')
                     ->setParameter('type', Vote::TYPE_LIKE)
                     ->groupBy('m.id')
                     ->orderBy('COUNT(v.id)', $sortOrder);
                 break;
 
             case 'hates':
-                $qb->leftJoin('m.votes', 'v', Join::WITH, 'v.type = :type')
+                $queryBuilder->leftJoin('m.votes', 'v', Join::WITH, 'v.type = :type')
                     ->setParameter('type', Vote::TYPE_HATE)
                     ->groupBy('m.id')
                     ->orderBy('COUNT(v.id)', $sortOrder);
                 break;
 
             case 'updated_at':
-                $qb->orderBy('m.updated_at', $sortOrder);
+                $queryBuilder->orderBy('m.updated_at', $sortOrder);
                 break;
 
             case 'created_at':
             default:
-                $qb->orderBy('m.created_at', $sortOrder);
+                $queryBuilder->orderBy('m.created_at', $sortOrder);
                 break;
         }
 
-        return $qb;
+        return $queryBuilder;
     }
-
-//    /**
-//     * @return Movie[] Returns an array of Movie objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Movie
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
